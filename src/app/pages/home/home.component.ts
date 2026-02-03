a88a535ba69105ce0425eb864530f4914317e3ef
itzisurii <itzIsurika08@gmail.com>
2026-02-03 16:48:01 +0530

    add Aboutme, Projects, and Contact components to HomeComponent

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
 import { LandingPageComponent } from "../../components/landing-page/landing-page.component";
+import { AboutmeComponent } from "../../components/aboutme/aboutme.component";
+import { ProjectsComponent } from "../../components/projects/projects.component";
+import { ContactComponent } from "../../components/contact/contact.component";
 
 @Component({
   selector: 'app-home',
-  imports: [NavBarComponent, LandingPageComponent],
+  imports: [NavBarComponent, LandingPageComponent, AboutmeComponent, ProjectsComponent, ContactComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
